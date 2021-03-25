1453630db85299f93c4615776031101975fbba57
Anis FATHALLAH <anis@fathallah.fr>
2021-03-25 12:06:51 +0400

    refactoring

--- gen_docs.py
+++ gen_docs.py
@@ -16,27 +16,31 @@ def add_signatures(tpl, company):
     company['shareholders'][i]['sign'] = InlineImage(tpl, sh['signature'], height=Mm(20))
   return company
 
-template_base_dir = Path('templates')
-operations = [child.name for child in template_base_dir.iterdir()]
+def get_operations(template_base_dir):
+  return [child.name for child in template_base_dir.iterdir()]
 
-parser = ArgumentParser(description='Supported operations are : {}'.format(', '.join(operations)))
-parser.add_argument('operation', type=str, help='templates folder to use from templates dir')
-args = parser.parse_args()
+def get_args(operations):
+  
+  parser = ArgumentParser(description='Supported operations are : {}'.format(', '.join(operations)))
+  parser.add_argument('operation', type=str, help='templates folder to use from templates dir')
+  args = parser.parse_args()
 
-if args.operation not in operations:
-  parser.print_help()
-  exit(1)
+  if args.operation not in operations:
+    parser.print_help()
+    exit(1)
+  
+  return args
 
-input_dir = PurePath('input')
-templates_dir = Path(template_base_dir, args.operation)
+def get_templates(templates_dir):
+  return sorted(templates_dir.glob('*.docx'))
 
-templates = sorted(templates_dir.glob('*.docx'))
+def load_yaml(path):
+  with open(path, 'r') as file:
+    return safe_load(file)
 
-with open(PurePath(input_dir, 'companies.yml'), 'r') as file:
-  parsed_yaml = safe_load(file)
-
-  for company in parsed_yaml["companies"]:
-    output_dir = Path('output', company['company_name'], args.operation)
+def main(yaml, templates, operation):
+  for company in yaml["companies"]:
+    output_dir = Path('output', company['company_name'], operation)
 
     if not output_dir.exists():
       Path.mkdir(output_dir, parents=True)
@@ -45,9 +49,17 @@ with open(PurePath(input_dir, 'companies.yml'), 'r') as file:
       print('{0}: {1}...'.format(company['company_name'], template.name ))
 
       tpl = DocxTemplate(template)
+
       tpl_context = add_signatures(tpl, company)
       tpl.render(tpl_context)
 
       tpl.save(PurePath(output_dir, template.name ))
 
       print('... Done !')
+
+if __name__ == '__main__':
+  possible_operations = get_operations(Path('templates'))
+  args = get_args(possible_operations)
+  templates = get_templates(PurePath('templates', args.operation))
+  yaml = load_yaml(PurePath('input', 'companies.yml'))
+  main(yaml, templates, args.operation)
